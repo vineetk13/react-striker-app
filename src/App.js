6149c1cd8960702ecada13c34849654f4550e1f6
vineetk13 <vineetkme.99@gmail.com>
2022-01-16 23:35:49 +0530

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { format, addDays } from "date-fns";
+
+import Check from "./assets/check-strike.svg";
+import Cross from "./assets/cancel-strike.svg";
+
+import {
+  Title,
+  Percent,
+  Strike,
+  CardContainer,
+  DayCard,
+  DateText,
+  Day,
+  Actions,
+  ActionButton,
+  LoaderContainer,
+  Loader
+} from "./styled";
 
 function App() {
+  const [strikeData, setStrikeData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false)
+  useEffect(() => {
+    fetch("https://vineet-striker-app.herokuapp.com/mystrike", {
+      method: "GET",
+      headers: {
+        Accept: "*/*",
+      },
+    })
+      .then((res) => res.json())
+      .then((jsonres) => setStrikeData(jsonres))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleAction = (hit) => {
+    setIsLoading(true)
+    let headersList = {
+      "Accept": "*/*",
+      "Content-Type": "application/json"
+     }
+    const body = {
+      hit: hit,
+      date: new Date().toISOString()
+    }
+
+    fetch("https://vineet-striker-app.herokuapp.com/mystrike", {
+      method: "PATCH",
+      headers: headersList,
+      body: JSON.stringify(body)
+    })
+    .then((res) => res.json())
+    .then((jsonres) => {
+      setStrikeData(jsonres)
+      setIsLoading(false)
+    })
+    .catch((err) => {
+      console.log(err)
+      setIsLoading(false)
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoading && 
+        <>
+          <LoaderContainer>
+          </LoaderContainer>
+          <Loader></Loader>
+        </>
+      }
+      <Title>Striker</Title>
+      <Strike>
+        {strikeData?.total ? Math.round((strikeData.strike/strikeData.total)*100) : 0}<Percent>%</Percent>
+      </Strike>
+      <CardContainer>
+        {strikeData !== null && strikeData.last_strike !== null ? (
+          <DayCard>
+            <DateText>
+              {format(new Date(strikeData?.last_strike?.date), "MMM d, yyyy") ||
+                "--"}
+            </DateText>
+            <Day>
+              {format(new Date(strikeData?.last_strike?.date), "EEEE") || "--"}
+            </Day>
+            <Actions>
+              <ActionButton>
+                <img src={Cross} />
+              </ActionButton>
+              <ActionButton>
+                <img src={Check} />
+              </ActionButton>
+            </Actions>
+          </DayCard>
+        ) : null}
+        <DayCard>
+          <DateText>{format(new Date(), "MMM d, yyyy") || "--"}</DateText>
+          <Day>{format(new Date(), "EEEE") || "--"}</Day>
+          <Actions>
+            <ActionButton onClick={() => handleAction(false)}>
+              <img src={Cross} />
+            </ActionButton>
+            <ActionButton onClick={() => handleAction(true)}>
+              <img src={Check} />
+            </ActionButton>
+          </Actions>
+        </DayCard>
+        <DayCard>
+          <DateText>
+            {format(addDays(new Date(), 1), "MMM d, yyyy") || "--"}
+          </DateText>
+          <Day>{format(addDays(new Date(), 1), "EEEE") || "--"}</Day>
+          <Actions>
+            <ActionButton>
+              <img src={Cross} />
+            </ActionButton>
+            <ActionButton>
+              <img src={Check} />
+            </ActionButton>
+          </Actions>
+        </DayCard>
+      </CardContainer>
     </div>
   );
 }
---
+++ src/styled.js
@@ -0,0 +1,104 @@
+import styled, {keyframes} from 'styled-components'
+
+export const Title = styled.h2`
+      font-family: 'Poppins', sans-serif;
+      // font-weight: bolder;
+      letter-spacing: 0.05em;
+      font-size:50px;
+`
+
+export const Strike = styled.p`
+      margin:0;
+      padding:0;
+      font-family: 'Lato', sans-serif;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 150px;
+      color: #36b37e;
+`
+export const Percent = styled.span`
+      font-family: 'Lato', sans-serif;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 80px;
+      margin-left:10px;
+`
+export const CardContainer = styled.div`
+      // border: 2px dashed red;
+      width: 100vw;
+      display: grid;
+      grid-template-columns: repeat(3, auto);
+      overflow-y: auto;
+      justify-content: center;
+      margin-top: 70px;
+      -ms-overflow-style: none;  
+      scrollbar-width: none;
+      &::-webkit-scrollbar {
+            display: none;
+      }
+      @media (max-width: 600px) {
+            justify-content: start;
+      }
+`
+export const DayCard = styled.div`
+      width: 150px;
+      margin: 0 15px;
+      border: 1px solid #CED4DA;
+      border-radius: 4px;
+      padding: 30px 20px;
+`
+export const DateText = styled.p`
+      margin: 0;
+      // margin-top: 15px;
+      font-family: 'Inter', sans-serif;
+      font-weight: 800;
+      font-size: 24px;
+`
+export const Day = styled.p`
+      margin:0;
+      margin-top: 10px;
+      margin-bottom: 25px;
+      font-family: 'Inter', sans-serif;
+      font-size: 24px;
+`
+export const Actions = styled.div``
+export const ActionButton = styled.button`
+      background: none;
+      border: none;
+      padding:0;
+      cursor:pointer;
+      margin: 0 8px;
+      outline: none;
+`
+const spin = keyframes`
+      0% { transform: rotate(0deg); }
+      100% { transform: rotate(360deg); }
+`;
+export const Loader = styled.div`
+      z-index:99;
+      position: absolute;
+      top: calc(50% - 30px);
+      left:calc(50% - 30px);
+      // font-family: 'Roboto', sans-serif;
+      // margin-right: -50%;
+      transform: translate(-40%, -40%);
+      border: 10px solid #f3f3f3;
+      filter: blur(0px);
+      border-radius: 50%;
+      border-top: 10px solid #3498db;
+      width: 60px;
+      // background: #ffff;
+      height: 60px;
+      -webkit-animation: ${spin} 1.5s linear infinite;
+      animation: ${spin} 1.5s linear infinite;
+`
+export const LoaderContainer = styled.div`
+      position: absolute;
+      width: 100%;
+      height: 100vh;
+      top: 0;
+      left: 0;
+      -webkit-backdrop-filter: blur(2px);
+      backdrop-filter: blur(2px);
+      background-color: rgba(0, 0, 0, 0.11);
+`
